0ebe476681577ee0de55f8b70277cb5e91ee963d
Blaze Rowland <browland.bdm@gmail.com>
2021-02-21 20:46:40 -0600

    Step one

--- 1/assets/js/step-one.js
+++ 1/assets/js/step-one.js
@@ -1,1 +1,46 @@
 // Step One
+
+var item = "test1";
+console.log(item);
+item = 1;
+console.log(item);
+
+// let item2 = "test2";
+// console.log(item2);
+// item = 2;
+// console.log(item2);
+
+// const item3 = "test3"
+// console.log(item3);
+// item3 = 3;
+// console.log(item3);
+
+// var test = 1;
+// function test() {
+//   var test = 2;
+//   return test;
+// }
+
+
+// What's expected here?
+// console.log(test());
+
+// const test = 2;
+// test = 1;
+// console.log(test);
+
+
+// Using functional arguments
+// function testTwo(something) {
+//   return something;
+// }
+// console.log(testTwo('test'));
+
+
+// Using arguments beneficially
+// function sum(a, b) {
+//   return a + b;
+// };
+// console.log(sum(2, 2));
+
+
